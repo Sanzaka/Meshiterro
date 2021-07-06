3072876a2bd26d046ae90f2715cadc7069bbdfe2
Sanzaka <ec2-user@ip-172-31-78-159.ec2.internal>
2021-07-06 08:14:12 +0000

    [end]4-4

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '2cae7a4fc145f4721c2c3d31cb91b418492b36b82a6036090ad7129b4122cba38c0fea35c51a8e089c1228f9edfa58c4f9ac1fbc1fc389cf06f608974bd230e0'
